42cd4122d3d5096f05afe63e88ad7a8caf52c27d
alekshiidenhovi <76514868+alekshiidenhovi@users.noreply.github.com>
2025-06-18 21:21:29 +0300

    New utility functions

--- src/common/utils.py
+++ src/common/utils.py
@@ -1,5 +1,7 @@
 import typing as T
 import click
+import datetime
+import uuid
 
 
 def parse_int_list(
@@ -8,3 +10,12 @@ def parse_int_list(
     if value is None:
         return None
     return [int(x.strip()) for x in value.split(",")]
+
+
+def get_experiment_name(max_depth: int, n_estimators: int) -> str:
+    current_datetime = datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
+    return f"{current_datetime}-training-time-benchmark-max_depth-{max_depth}-n_estimators-{n_estimators}"
+
+
+def get_benchmark_id() -> str:
+    return str(uuid.uuid4())
