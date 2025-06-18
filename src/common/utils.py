4c7078c337c056429366e601a60a60c46290f979
alekshiidenhovi <76514868+alekshiidenhovi@users.noreply.github.com>
2025-06-18 15:57:13 +0300

    Add parse_int_list utility

--- src/common/utils.py
+++ src/common/utils.py
@@ -1,9 +1,10 @@
-from common.config import TrainingConfig
+import typing as T
+import click
 
 
-def get_experiment_group_name(
-    current_datetime: str,
-    training_config: TrainingConfig,
-):
-    experiment_group_name = f"{current_datetime}-xgb_train-{training_config.n_estimators}_noftrees-{training_config.max_depth}_depth-{training_config.learning_rate:.3f}_lr-{training_config.subsample:.3f}_subsample-{training_config.colsample_bytree:.3f}_colsample"
-    return experiment_group_name
+def parse_int_list(
+    ctx: click.Context, param: click.Option, value: T.Optional[str]
+) -> T.Optional[T.List[int]]:
+    if value is None:
+        return None
+    return [int(x.strip()) for x in value.split(",")]
