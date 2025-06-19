421ec4e663c54b2a4fe78faf40c111d67d1ef834
alekshiidenhovi <76514868+alekshiidenhovi@users.noreply.github.com>
2025-06-19 16:33:51 +0300

    Create pathing utils

---
+++ src/benchmarking/pathing.py
@@ -0,0 +1,75 @@
+import os
+
+
+def get_local_settings_config_file_path(
+    dir_path: str,
+    benchmark_id: str,
+    file_name: str,
+) -> str:
+    return os.path.join(
+        dir_path,
+        benchmark_id,
+        file_name,
+    )
+
+
+def get_local_training_config_file_path(
+    dir_path: str,
+    benchmark_name: str,
+    experiment_name: str,
+    benchmark_id: str,
+    file_name: str,
+) -> str:
+    return os.path.join(
+        dir_path,
+        benchmark_name,
+        benchmark_id,
+        experiment_name,
+        file_name,
+    )
+
+
+def get_local_model_file_path(
+    dir_path: str,
+    benchmark_name: str,
+    experiment_name: str,
+    benchmark_id: str,
+    file_name: str,
+) -> str:
+    return os.path.join(
+        dir_path,
+        benchmark_name,
+        benchmark_id,
+        experiment_name,
+        file_name,
+    )
+
+
+def get_training_time_metrics_file_path(
+    dir_path: str,
+    benchmark_name: str,
+    benchmark_id: str,
+) -> str:
+    return os.path.join(
+        dir_path,
+        benchmark_name,
+        benchmark_id,
+        "training_time_metrics.parquet",
+    )
+
+
+def get_explanation_time_metrics_file_path(
+    dir_path: str,
+    benchmark_name: str,
+    benchmark_id: str,
+) -> str:
+    return os.path.join(
+        dir_path,
+        benchmark_name,
+        benchmark_id,
+        "explanation_time_metrics.parquet",
+    )
+
+
+def create_dir_if_not_exists(file_path: str):
+    os.makedirs(os.path.dirname(file_path), exist_ok=True)
