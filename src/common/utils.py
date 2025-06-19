6d6a5ce4e94820ba7522b88f7275a094c4b09332
alekshiidenhovi <76514868+alekshiidenhovi@users.noreply.github.com>
2025-06-19 09:52:16 +0300

    Add new current datetime utility

--- src/common/utils.py
+++ src/common/utils.py
@@ -13,8 +13,12 @@ def parse_int_list(
     return [int(x.strip()) for x in value.split(",")]
 
 
+def get_current_datetime() -> str:
+    return datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
+
+
 def get_experiment_name(max_depth: int, n_estimators: int) -> str:
-    current_datetime = datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
+    current_datetime = get_current_datetime()
     return f"{current_datetime}-training-time-benchmark-max_depth-{max_depth}-n_estimators-{n_estimators}"
 
 
